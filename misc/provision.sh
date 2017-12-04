d92622dbb427a0a87ca082119a44215128bb1872
happlebao <bao.github@outlook.com>
2017-12-05 04:52:20 +0800

    utf8

--- misc/provision.sh
+++ misc/provision.sh
@@ -1,5 +1,7 @@
+set -ex
+sudo apt-get update
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get update
 sudo apt-get install -y python3.6 python3-pip nginx
 sudo -H python3.6 -m pip install sqlalchemy pyquery
-sudo ln -s /var/www/rank/misc/rank.service /etc/systemd/system/rank.service
+sudo ln -f -s /var/www/rank/misc/rank.service /etc/systemd/system/rank.service
