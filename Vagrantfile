a219940eb2ab6780465558facb404c6e2463c4d1
happlebao <bao.github@outlook.com>
2017-12-05 22:42:34 +0800

    change mirror

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,7 @@
 # vi: set ft=ruby :
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
+  config.vm.box_check_update = false
   config.vm.network "public_network"
   config.vm.synced_folder ".", "/var/www/rank"
   config.vm.provision "shell", inline: <<-SHELL
--- misc/provision.sh
+++ misc/provision.sh
@@ -1,9 +1,15 @@
 set -ex
+
+ln -f -s /var/www/rank/misc/ubuntu.list /etc/apt/sources.list
+mkdir -p /root/.pip
+ln -f -s /var/www/rank/misc/pip.conf /root/.pip/pip.conf
+
 sudo apt-get update
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get update
 sudo apt-get install -y python3.6 python3-pip nginx
 sudo -H python3.6 -m pip install sqlalchemy pyquery
+
 sudo ln -f -s /var/www/rank/misc/rank.service /etc/systemd/system/rank.service
 sudo ln -f -s /var/www/rank/misc/rank.timer /etc/systemd/system/rank.timer
 sudo systemctl daemon-reload
