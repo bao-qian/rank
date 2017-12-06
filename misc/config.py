82ba7d3531d9cd2840705943aa2bbf0a74646641
happlebao <bao.github@outlook.com>
2017-12-07 02:49:48 +0800

    add validation for name and description

--- misc/config.py
+++ misc/config.py
@@ -6,7 +6,7 @@ user_query_and_count = [
     ('location:PRC followers:>=100', 1000),
 ]
 count_per_request = 100
-cache_time = 10800
+cache_time = 36000
 
 root = os.path.dirname(os.path.dirname(__file__))
 static = os.path.join(root, 'static')
@@ -20,6 +20,7 @@ extra_user = [
 ]
 
 invalid_language = [
+    'reStructuredText',
     'Jupyter Notebook',
     'Graphviz (DOT)',
     'MediaWiki',
@@ -29,4 +30,31 @@ invalid_language = [
     'Text',
     'SVG',
     'TeX',
+]
+
+invalid_description = [
+    'interview',
+    'tutorial',
+    'leetcode',
+    'study',
+    'tips',
+    '学习笔记',
+    '学习资源',
+    '开发实战',
+    '交流群',
+    '学习群',
+    '实战',
+    '精华',
+    '入门',
+    '书籍',
+    '教程',
+    '学习',
+    '系列',
+    '面试',
+    '集锦',
+    '文章',
+    '解读',
+    '指南',
+    '讲解',
+    '全书',
 ]
