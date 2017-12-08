b8017a6daa8498997def9b6f128a1c73c8f03409
happlebao <bao.github@outlook.com>
2017-12-09 03:41:32 +0800

    refactoring contribution

--- source/contribution.py
+++ source/contribution.py
@@ -21,32 +21,39 @@ class Contribution(Model):
         self.login = login
         self.valid = False
 
-        repository.validate_code()
-        if repository.valid:
-            log('valid code repo', repository.name_with_owner)
-            q = Repository.query_for_contributors(repository.name_with_owner)
+    def add_commit(self):
+        q = Repository.query_for_contributors(self.repository.name_with_owner)
 
-            try:
-                cs = API.get_v3(q)
-            except HTTPError:
-                cs = []
+        try:
+            cs = API.get_v3(q)
+        except HTTPError:
+            cs = []
 
-            # only for last x year
-            past = int(time.time()) - int(365 * 24 * 3600 * config.contribution_year)
-            for c in cs:
-                author = c['author']
-                # https://api.github.com/repos/iview/iview/stats/contributors
-                # author may be null
-                if author is not None:
-                    _login = c['author']['login']
-                    weeks = c['weeks']
-                    for w in weeks:
-                        week_start = int(w['w'])
-                        if week_start > past and w['c'] > 0:
-                            self.total_commit += w['c']
-                            if _login == login:
-                                self.commit += w['c']
+        # only for last x year
+        past = int(time.time()) - int(365 * 24 * 3600 * config.contribution_year)
+        for c in cs:
+            author = c['author']
+            # https://api.github.com/repos/iview/iview/stats/contributors
+            # author may be null
+            if author is not None:
+                _login = c['author']['login']
+                weeks = c['weeks']
+                for w in weeks:
+                    week_start = int(w['w'])
+                    if week_start > past and w['c'] > 0:
+                        self.total_commit += w['c']
+                        if _login == self.login:
+                            self.commit += w['c']
 
+    def add_star(self):
+        self.rate = self.commit / self.total_commit
+        self.star = int(self.repository.start_count * self.rate)
+
+    def validate(self):
+        self.repository.validate()
+        if self.repository.valid:
+            log('valid code repo', self.repository.name_with_owner)
+            self.add_commit()
             # at least x commit
             if self.login == self.repository.owner and self.commit > 3:
                 self.valid = True
@@ -55,21 +62,17 @@ class Contribution(Model):
             else:
                 self.valid = False
 
-            if self.valid:
-                self.rate = self.commit / self.total_commit
-                self.star = int(repository.start_count * self.rate)
-            else:
+            if not self.valid:
                 self.all_invalid.append(
-                    (repository.name_with_owner, self.commit, self.total_commit)
+                    (self.repository.name_with_owner, self.commit, self.total_commit)
                 )
-
-            log('valid code repo contribution <{}> <{}> <{}> <{}>'.format(
-                self.commit, self.total_commit, self.star, self.repository.start_count)
-            )
 
     @classmethod
     def all(cls, login, repositories):
         for r in repositories:
             c = Contribution(login, r)
-            if c.valid and c.star > 0:
-                yield c
+            c.validate()
+            if c.valid:
+                c.add_star()
+                if c.star > 0:
+                    yield c
