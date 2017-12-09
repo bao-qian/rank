b2d93b885b07be2f3b63ec0a85b9c937872e59b5
happlebao <bao.github@outlook.com>
2017-12-09 17:13:30 +0800

    extract base_config

---
+++ misc/base_config.py
@@ -0,0 +1,53 @@
+import os
+
+root = os.path.dirname(os.path.dirname(__file__))
+static = os.path.join(root, 'static')
+misc = os.path.join(root, 'misc')
+template = os.path.join(root, 'template')
+
+block_user = [
+    'diwu',
+    'xuzhezhaozhao',
+    'rover12421',
+    'tiankon',
+]
+
+invalid_language = [
+    'reStructuredText',
+    'Jupyter Notebook',
+    'Graphviz (DOT)',
+    'MediaWiki',
+    'Markdown',
+    'YAML',
+    'HTML',
+    'Text',
+    'SVG',
+    'TeX',
+]
+
+invalid_description = [
+    'interview',
+    'tutorial',
+    'study',
+    'tips',
+    '学习笔记',
+    '读书笔记',
+    '学习资源',
+    '开发实战',
+    '交流群',
+    '学习群',
+    '实战',
+    '精华',
+    '入门',
+    '书籍',
+    '教程',
+    '学习',
+    '系列',
+    '面试',
+    '集锦',
+    '文章',
+    '解读',
+    '指南',
+    '讲解',
+    '全书',
+]
--- misc/server_config.py
+++ misc/server_config.py
@@ -1,4 +1,4 @@
-import os
+from misc.base_config import *
 
 user_query_and_count = [
     ('location:china followers:>200', 1000),
@@ -23,56 +23,4 @@ extra_user = [
     'xinzhengzhang',
     'zhouxinyu',
     'rebornix',
-]
-
-block_user = [
-    'diwu',
-    'xuzhezhaozhao',
-    'rover12421',
-    'tiankon',
-]
-
-root = os.path.dirname(os.path.dirname(__file__))
-static = os.path.join(root, 'static')
-misc = os.path.join(root, 'misc')
-template = os.path.join(root, 'template')
-
-invalid_language = [
-    'reStructuredText',
-    'Jupyter Notebook',
-    'Graphviz (DOT)',
-    'MediaWiki',
-    'Markdown',
-    'YAML',
-    'HTML',
-    'Text',
-    'SVG',
-    'TeX',
-]
-
-invalid_description = [
-    'interview',
-    'tutorial',
-    'study',
-    'tips',
-    '学习笔记',
-    '读书笔记',
-    '学习资源',
-    '开发实战',
-    '交流群',
-    '学习群',
-    '实战',
-    '精华',
-    '入门',
-    '书籍',
-    '教程',
-    '学习',
-    '系列',
-    '面试',
-    '集锦',
-    '文章',
-    '解读',
-    '指南',
-    '讲解',
-    '全书',
 ]
