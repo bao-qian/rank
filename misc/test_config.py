f7237153cab5f82c8c2a38e18b6296591185044a
happlebao <bao.github@outlook.com>
2017-12-10 00:21:52 +0800

    fixup

--- misc/test_config.py
+++ misc/test_config.py
@@ -1,17 +1,20 @@
 # noinspection PyUnresolvedReferences
 from misc.base_config import *
+import time
+
 
 user_query_and_count = [
     ('location:china followers:>200', 1),
     ('location:china followers:100..200', 1),
     ('location:PRC followers:>=100', 1),
 ]
-count_per_request = 1
+count_per_request = 100
 cache_time = 36000000000000000
 contribution_year = 3
-past = int(time.time()) - int(365 * 24 * 3600 * config.contribution_year)
+valid_from = int(time.time()) - int(365 * 24 * 3600 * contribution_year)
 
 extra_user = [
+    'happlebao',
     'vczh',
 ]
 
